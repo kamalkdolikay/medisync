80c26719c5b8e8cdbfd35cd79a8628b4897b59c3
Kamal Dolikay <kamaldolikay@gmail.com>
2025-01-18 13:21:18 -0800

    project structure and flow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,17 @@
+// App.tsx
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+// Importing page components
+import HomeComponent from "./pages/home/home-component";
+
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+        <Routes>
+            {/* Unprotected routes */}
+            <Route path='/' element={<HomeComponent />} />
+        </Routes>
+    </Router>
   )
 }
 
