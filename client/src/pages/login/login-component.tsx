ef30237b8dfb44e252f6f2ed6a7d33f561fc4f69
Kamal Dolikay <kamaldolikay@gmail.com>
2025-01-19 11:03:21 -0800

    login form design and api implementation

--- client/src/pages/login/login-component.tsx
+++ client/src/pages/login/login-component.tsx
@@ -1,8 +1,40 @@
-// HomeComponent.tsx
-import HomeHtml from './home-html';
+// LoginComponent.tsx
+import React, { useEffect, useState } from 'react';
+import { useAuth } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import { login } from '../../services/authService';
 
-const HomeComponent = () => {
-  return (<HomeHtml />);
+import LoginHtml from "./login-html";
+
+const LoginComponent = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const { login: authLogin, token } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (token) {
+          navigate('/dashboard'); // Redirect if already logged in
+        }
+      }, [token, navigate]);
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            const data = await login(email, password);
+            authLogin(data.token); // Save the token in context and localStorage
+            navigate('/users'); // Redirect to users page after login
+        } catch (error: any) {
+            alert(error.response?.data?.error || 'Login failed');
+        }
+    };
+
+  return (<LoginHtml
+            handleSubmit={handleSubmit}
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword} />);
 };
 
-export default HomeComponent;
+export default LoginComponent;
--- client/src/pages/login/login-html.tsx
+++ client/src/pages/login/login-html.tsx
@@ -1,93 +1,54 @@
-const HomeHtml = () => {
-  return (
-    <div>
-      <div id="nav-placeholder"></div>
-      <div className="container">
-        <div className="card bg-a8dadc p-4 mb-4 border-0 rounded-4">
-          <h4 className="text-center mb-3">1. Tell Us About Your Medication</h4>
-          <div className="mb-3">
-            <label htmlFor="name" className="form-label">Medication Name:</label>
-            <input type="text" className="form-control" id="name" required />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="type" className="form-label">Type of Medication:</label>
-            <input type="text" list="datalistOptions" className="form-control" id="type" placeholder="Type to search..." required />
-            <datalist id="datalistOptions">
-              <option value="Vitamin" />
-              <option value="Ibuprofen" />
-              <option value="Antidepressant" />
-              <option value="Antibiotic" />
-              <option value="Supplement" />
-              <option value="Ointment" />
-              <option value="Eye drops" />
-              <option value="Nasal Spray" />
-            </datalist>
-          </div>
-          <label htmlFor="dose" className="form-label">Dosage:</label>
-          <div className="input-group mb-3">
-            <input type="number" className="form-control" id="dose" value="1" max="10" min="1" required />
-            <button id="add1" className="btn btn-outline-secondary bg-457b9d c-f1faee" type="button">
-              <i className="bi bi-caret-up"></i>
-            </button>
-            <button id="remove1" className="btn btn-outline-secondary bg-457b9d c-f1faee" type="button">
-              <i className="bi bi-caret-down"></i>
-            </button>
-          </div>
-          <div>
-            <label htmlFor="desc" className="form-label">Personal Notes:</label>
-            <textarea id="desc" className="form-control" rows={5} maxLength={100}></textarea>
-          </div>
-        </div>
-        <div className="card bg-a8dadc p-4 mb-4 border-0 rounded-4">
-          <h4 className="text-center mb-3">2. When Would You Like To Take It?</h4>
-          <div className="mb-3">
-            <label htmlFor="date" className="form-label">Select Day(s):</label>
-            <div className="d-flex justify-content-evenly align-items-center">
-              <button type="button" className="btn day-btn bg-457b9d c-f1faee remove-border" data-bs-toggle="button" value="0">S</button>
-              <button type="button" className="btn day-btn bg-457b9d c-f1faee remove-border" data-bs-toggle="button" value="1">M</button>
-              <button type="button" className="btn day-btn bg-457b9d c-f1faee remove-border" data-bs-toggle="button" value="2">T</button>
-              <button type="button" className="btn day-btn bg-457b9d c-f1faee remove-border" data-bs-toggle="button" value="3">W</button>
-              <button type="button" className="btn day-btn bg-457b9d c-f1faee remove-border" data-bs-toggle="button" value="4">T</button>
-              <button type="button" className="btn day-btn bg-457b9d c-f1faee remove-border" data-bs-toggle="button" value="5">F</button>
-              <button type="button" className="btn day-btn bg-457b9d c-f1faee remove-border" data-bs-toggle="button" value="6">S</button>
-            </div>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="time" className="form-label">Select a time:</label>
-            <div className="cs-form">
-              <input id="time" type="time" className="form-control" required />
-            </div>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="repeat" className="form-label">Repeating:</label>
-            <select className="form-select" aria-label="Default select example" id="repeat">
-              <option selected value="weekly">Weekly</option>
-              <option value="daily">Daily</option>
-            </select>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="endDate">End Date</label>
-            <input id="endDate" className="form-control" type="date" required />
-          </div>
-        </div>
-        <div className="card bg-a8dadc p-4 border-0 rounded-4">
-          <h4 className="text-center mb-3">3. Would You Like Us To Save a Picture For You?</h4>
-          <p className="text-center"><i>Just in case you forget (optional)</i></p>
-          <div className="text-center">
-            <i className="bi bi-camera c-1d3557"></i>
-            <div className="text-center">
-              <input type="file" id="medImg-1" accept="image/png, image/jpeg" />
-              <label htmlFor="medImg-1" className="file-btn">Choose Image</label>
-            </div>
-            <p id="image-conf-msg" className="mt-3" hidden>Image uploaded<i className="bi bi-check"></i></p>
-          </div>
-        </div>
-        <div className="mt-3 text-center">
-          <button type="submit" className="btn btn-primary bg-1d3557 c-f1faee remove-border save-btn rounded-3">Save</button>
-        </div>
-      </div>
-    </div>
-  );
+import logo from "../../assets/full-transparent-logo.png"
+import './login.css';
+
+type LoginHtmlProps = {
+    handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+    email: string;
+    setEmail: (value: string) => void;
+    password: string;
+    setPassword: (value: string) => void;
 };
 
-export default HomeHtml;
+const LoginHtml: React.FC<LoginHtmlProps> = ({handleSubmit, email, setEmail, password, setPassword}) => {
+  return (
+    <>
+        <img src={logo} style={{ width: "300px", height: "300px", marginTop: "50px", marginBottom: "0px" }}></img>
+        <form onSubmit={handleSubmit}>
+            <div className="content-container">
+            <h2 className="text-center">Please login</h2>
+            <div className="form-group">
+            {/* <label htmlFor="medicationName" className="form-label">
+                Email:
+            </label> */}
+            <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder='Email'
+                required
+                />
+            </div>
+            <div className="form-group">
+            {/* <label htmlFor="refillAlert" className="form-label">
+                Password:
+            </label> */}
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder='Password'
+                required
+                />
+            </div>
+            <div className="text-center mt-4">
+            <button>
+                Login
+            </button>
+            </div>
+        </div>
+        </form>
+    </>
+  )
+}
+
+export default LoginHtml
