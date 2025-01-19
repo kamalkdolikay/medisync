54db5efb629badef11f8182c56c29984a7e96f47
Tianyou Xie <tianyouxie001@gmail.com>
2025-01-19 02:07:00 -0800

    Added link to view-prescription page.

--- client/src/pages/view-prescription/view-prescription-html.tsx
+++ client/src/pages/view-prescription/view-prescription-html.tsx
@@ -37,7 +37,7 @@ const ViewPrescription = () => {
       icon: "info",
       confirmButtonText: "Okay",
     }).then(() => {
-      navigate("/add-prescription");
+      navigate("/medicine-input");
     });
   };
 
