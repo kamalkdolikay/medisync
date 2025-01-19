5f1fbdbff45443a46d4a72316eedf9676500bbcd
ravkrandhawa <rrandhawa59@my.bcit.ca>
2025-01-18 18:36:21 -0800

    created home page

--- client/src/pages/home/home-html.tsx
+++ client/src/pages/home/home-html.tsx
@@ -1,99 +1,86 @@
 // HomeHtml.tsx
+import refill from "../../assets/refill.png";
+import reminder from "../../assets/reminder.png";
+import order from "../../assets/order.png"
+import logo from "../../assets/logo2.png"
+import doctor from "../../assets/doctor.png"
 import './home.css';
 
 const HomeHtml = () => {
     return (
-      <>
-       <nav className="navbar navbar-expand-md bg-primary-subtle fixed-top border-bottom" role="navigation">
-            <div className="container">
-                <a className="navbar-brand d-md-none" href="/index.html">
-                {/* <img className="bi" width="35" height="35" src="../images/MentalWellnessLogo.png" /> */}
-                </a>
-                <a className="navbar-brand d-md-none" href="/index.html">
-                <span>Med Sync</span>
-                </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas"
-                aria-controls="offcanvas" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="offcanvas offcanvas-end" id="offcanvas" aria-labelledby="offcanvasLabel">
-                <div className="offcanvas-header">
-                    <h5 className="offcanvas-title" id="offcanvasLabel">Menu</h5>
-                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <>
+            <nav className="navbar navbar-expand-md fixed-top border-bottom" style={{ backgroundColor: "#d9d9d9" }} role="navigation">
+                <div className="container">
+                    <a className="navbar-brand d-md-none" href="/index.html">
+                        {/* <img className="bi" width="35" height="35" src="../images/MentalWellnessLogo.png" /> */}
+                    </a>
+                    <a className="navbar-brand d-md-none" href="/index.html">
+                        <span>MediSync</span>
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas"
+                        aria-controls="offcanvas" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="offcanvas offcanvas-end" id="offcanvas" aria-labelledby="offcanvasLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasLabel">Menu</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div className="offcanvas-body">
+                            <ul className="navbar-nav flex-grow-1 justify-content-between">
+                                <li className="nav-item"><a className="nav-link" href="/index.html"><span>Home</span></a></li>
+                                <li className="nav-item"><a className="nav-link" href="/about.html"><span>About</span></a></li>
+                                {/* <a href="/index.html"><img className="bi1" width="40" height="40" src="../images/MentalWellnessLogo.png" /></a> */}
+                                <li className="nav-item"><a className="nav-link" href="/faq.html"><span>FAQ's</span></a></li>
+                                <li className="nav-item"><a className="nav-link custom-nav-link">Login</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div className="offcanvas-body">
-                    <ul className="navbar-nav flex-grow-1 justify-content-between">
-                    <li className="nav-item"><a className="nav-link" href="/index.html"><span>Home</span></a></li>
-                    <li className="nav-item"><a className="nav-link" href="/about.html"><span>About</span></a></li>
-                    {/* <a href="/index.html"><img className="bi1" width="40" height="40" src="../images/MentalWellnessLogo.png" /></a> */}
-                    <li className="nav-item"><a className="nav-link" href="/faq.html"><span>FAQ's</span></a></li>
-                    <li className="nav-item"><a className="nav-link custom-nav-link">Login</a></li>
-                    </ul>
-                </div>
-                </div>
+            </nav>
+
+            <div>
+                <img src={logo} style={{ width: "200px", height: "200px", marginTop: "0px", marginBottom: "0px" }}></img>
+                <h1 id="name-goes-here">Hi, Kamal</h1>
             </div>
-        </nav>
 
-        <div className="py-5 mt-5 text-center">
-            <div className="col-lg-6 mx-auto mb-4">
-                {/* <img id="img" alt="profile picture" style="height: 35%; width: 35%;" src="./images/MentalWellnessLogo.png" /> */}
+            <div className="container mb-3">
+                <input className="form-control me-2 custom-rounded" type="search" placeholder="Search" aria-label="Search" />
             </div>
-            <h1 className="display-4" id="name-goes-here">Hi, Kamal</h1>
-            
-            <hr />
-            <p className="display-6">How are you feeling today?</p>
-        </div>
 
-        <div className="container mb-3">
-            <input className="form-control me-2 custom-rounded" type="search" placeholder="Search" aria-label="Search" />
-            {/* <textarea className="form-control form-control-lg" id="message" type="text" placeholder="Enter your thoughts" aria-label=".form-control-lg example"></textarea> */}
-        </div>
 
-        <div className="d-flex gap-2 justify-content-center pb-5">
-        <button className="btn btn-primary rounded-circle p-3 lh-1" type="button">
-            <svg className="bi" width="24" height="24"></svg>
-            <span className="visually-hidden">Dismiss</span>
-        </button>
-        <button className="btn btn-outline-primary rounded-circle p-3 lh-1" type="button">
-            <svg className="bi" width="24" height="24"></svg>
-            <span className="visually-hidden">Dismiss</span>
-        </button>
-        <button className="btn btn-primary rounded-circle p-3 lh-1" type="button">
-            <svg className="bi" width="24" height="24"></svg>
-            <span className="visually-hidden">Dismiss</span>
-        </button>
-        <button className="btn btn-outline-primary rounded-circle p-3 lh-1" type="button">
-            <svg className="bi" width="24" height="24"></svg>
-            <span className="visually-hidden">Dismiss</span>
-        </button>
-        </div>
+            <div className="icons">
+                <table>
+                    <tbody>
+                        <tr>
+                            <td className="icon-cell"><img src={order} style={{ width: "60px", height: "60px" }} alt="Order Icon" /></td>
+                            <td className="icon-cell"><img src={refill} style={{ width: "60px", height: "60px" }} alt="Refill Icon" /></td>
+                            <td className="icon-cell"><img src={reminder} style={{ width: "60px", height: "60px" }} alt="Reminder Icon" /></td>
+                            <td className="icon-cell"><img src={refill} style={{ width: "60px", height: "60px" }} alt="Refill Icon" /></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div> 
 
-        <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
-            <div className="col">
-            <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" >
-                <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Short title, long jacket</h3>
-                <ul className="d-flex list-unstyled mt-auto">
-                    <li className="me-auto">
-                    <img src="https://github.com/twbs.png" alt="Bootstrap" width="32" height="32" className="rounded-circle border border-white" />
-                    </li>
-                    <li className="d-flex align-items-center me-3">
-                    <svg className="bi me-2" width="1em" height="1em"></svg>
-                    <small>Earth</small>
-                    </li>
-                    <li className="d-flex align-items-center">
-                    <svg className="bi me-2" width="1em" height="1em"></svg>
-                    <small>3d</small>
-                    </li>
-                </ul>
+
+            <div className="card-container">
+                <div className="card-content">
+                    <div className="card-text">
+                        <h3><b>Contact Consultant</b></h3>
+                    </div>
+                    <div className="card-details">
+                        <button className="contact-button">
+                            Contact
+                        </button>
+                        <div className="card-icon">
+                            <img src={doctor} alt="Consultant Icon" />
+                        </div>
+                    </div>
                 </div>
             </div>
-            </div>
-      </div>
-        
-      </>
+
+        </>
     );
 };
-  
-  export default HomeHtml;
-  
+
+export default HomeHtml
